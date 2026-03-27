47674b31632c1d256e777e20cd259edaac6fbf41
sifatullah <s2410476116.ru.ac.bd>
2026-03-27 17:28:21 +0600

    B solution

---
+++ B.cpp
@@ -0,0 +1,123 @@
+/* ---------------------------------------------------In the name of ALLAH --------------------------------------------------*/
+/* ------------------------------------------LA ILAHA ILLALLAH MUHAMMAD RASHULALLAH------------------------------------------*/
+/* ---------------------------------------------------- ALLAHU AKBAR --------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* ---------- Typedefs ---------- */
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef set<int> si;
+typedef set<ll> sl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+typedef float fl;
+
+/* ---------- Macros ---------- */
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)(x).size()
+#define mid(l, r) ((l + r) / 2)
+#define left(node) ((node) * 2)
+#define right(node) ((node) * 2 + 1)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define sqr(a) ((a) * (a))
+
+/* ---------- Constants ---------- */
+const double PI = 3.141592653;
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000LL;
+const ll MOD = 1000000007;
+const int mx_int_prime = 999999937;
+
+/* ---------- Fast IO ---------- */
+#define optimize()               \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+/* ---------- Iterators ---------- */
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+/* ---------- Direction Arrays ---------- */
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+// int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
+// int dy[] = {0, 1, 0, -1, 1, -1, 1, -1};
+
+// const int mx = 1e5;
+vl v;
+/*For Division 1: 1900≤rating
+For Division 2: 1600≤rating≤1899
+For Division 3: 1400≤rating≤1599
+For Division 4: rating≤1399
+*/
+ll solve1()
+{
+    ll t = 3, ans = infLL;
+    while (t--)
+    {
+        ll sum = 0;
+        string st;
+        cin >> st;
+        for (ll i = 0; i < 3; i++)
+        {
+            if (st[i] == '?')
+            {
+                continue;
+            }
+            sum += (st[i]);
+        }
+        ans = min(ans, sum);
+    }
+    return 198 - ans;
+}
+void print(ll n)
+{
+
+    cout << (char)n;
+    cout << endl;
+}
+
+int main()
+{
+    optimize();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        ll m, n, a, b, c, c1 = 0, c2 = 0;
+        //  cin >> n >> a >> b;
+        // v.resize(n);
+
+        print(solve1());
+        // cout << endl;
+        //  v.clear();
+    }
+
+    return 0;
+}
